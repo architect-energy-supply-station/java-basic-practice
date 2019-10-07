01ebea993a757224b7bd035c417486c61793482a
Sunshinezmh <18333640045@163.com>
2019-10-07 19:54:01 +0800

    FileTest文件内容完善--张明慧--2019年10月07日19:53:54

--- io/io/FileTest.java
+++ io/io/FileTest.java
@@ -14,16 +14,13 @@ import java.io.IOException;
  * @Blog https://blog.csdn.net/t131452n?viewmode=contents
  */
 public class FileTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		String filePath = "/Users/sunshinezhang/Documents/Team3/code/java-basic-practice/io/file/";
+
 		//以当前路径创建一个file对象
 		File file = new File(filePath + "test.txt");
-		try {
-			file.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		file.createNewFile();
 
 		//获取文件名
 		System.out.println("获取文件名: " + file.getName());
@@ -35,27 +32,21 @@ public class FileTest {
 		System.out.println("获取绝对路径: " + file.getAbsolutePath());
 
 		//获取上一级路径
-		System.out.println("获取上一级路径: " + file.getParent());
+		System.out.println("获取上一级路径: " + file.getAbsoluteFile().getParent());
 
 		//在当前路径创建临时文件
-		try {
-			System.out.println("在当前路径创建临时文件： " + File.createTempFile("sun", ".word", new File(filePath)));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+		File tempFile = File.createTempFile("sun", ".word", new File(filePath));
+		System.out.println("在当前路径创建临时文件： " + tempFile.getName());
+
 
 		//当jvm退出时，自动删除该文件
-		try {
-			File.createTempFile("sunshine", ".txt", new File(filePath)).deleteOnExit();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		tempFile.deleteOnExit();
 
 		//以当前系统时间为文件名创建一个新的文件夹
-		File file1 = new File(String.valueOf(System.currentTimeMillis()));
+		File fileDir = new File(String.valueOf(System.currentTimeMillis()));
 		System.out.println("以当前系统时间为文件名创建一个新的文件夹");
-		file1.mkdir();
+		fileDir.mkdir();
 
 
 		//判断文件对象是否存在
@@ -64,12 +55,8 @@ public class FileTest {
 		}
 
 		//用指定的file对象创建一个文件名为filetest.txt的文件
-		File file2 = new File(filePath + "filetest.txt");
-		try {
-			file2.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		File fileTest = new File(filePath + "filetest.txt");
+		fileTest.createNewFile();
 
 		//用指定的file对象创建一个文件目录
 		File file3 = new File(filePath + "sunshine");
@@ -78,15 +65,15 @@ public class FileTest {
 
 
 		//列出当前路径下的所有文件和路径
-		File fileDir = new File(filePath);
-		for (String s : fileDir.list()) {
+		File fileDirList = new File(filePath);
+		for (String s : fileDirList.list()) {
 			System.out.println("列出当前路径下的所有文件和路径: " + s);
 		}
 
 		//列出所有的磁盘根路径
-		File[] files = File.listRoots();
-		for (File file4 : files) {
-			System.out.println(file4);
+		File[] filesRoots = File.listRoots();
+		for (File file4 : filesRoots) {
+			System.out.println("列出所有的磁盘根路径: "+file4);
 		}
 	}
 
